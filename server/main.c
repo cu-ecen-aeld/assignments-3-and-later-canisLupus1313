f3a5752793e98a862aedc1d963fa7beccf2d0e82
Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
2024-12-24 00:55:01 +0100

    assignment 6 p1

--- server/main.c
+++ server/main.c
@@ -1,12 +1,12 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/syslog.h>
@@ -17,187 +17,252 @@
 #define BUFFER_SIZE 1024
 FILE *fptr;
 
-int server_fd;     // Global server socket to close on shutdown
+int server_fd;	   // Global server socket to close on shutdown
 fd_set active_fds; // Global file descriptor set for cleanup
 char filepath[] = "/var/tmp/aesdsocketdata";
 char filebuf[30000];
 size_t loc = 0;
+FILE *fptr;
 
-void sig_handler(int param) {
-  syslog(LOG_WARNING, "Signal received %d.", param);
-  // Close all active client sockets
-  for (int fd = 0; fd < FD_SETSIZE; fd++) {
-    if (FD_ISSET(fd, &active_fds)) {
-      close(fd);
-    }
-  }
+void sig_handler(int param)
+{
+	syslog(LOG_WARNING, "Signal received %d.", param);
+	// Close all active client sockets
+	for (int fd = 0; fd < FD_SETSIZE; fd++) {
+		if (FD_ISSET(fd, &active_fds)) {
+			close(fd);
+		}
+	}
 
-  // Close the server socket
-  close(server_fd);
+	// Close the server socket
+	close(server_fd);
 
-  if (fptr != NULL) {
-    fclose(fptr);
-  }
-  unlink(filepath);
-  // Exit the program
-  exit(0);
+	if (fptr != NULL) {
+		fclose(fptr);
+	}
+	unlink(filepath);
+	// Exit the program
+	exit(0);
+}
+
+void *periodic_function(void *arg)
+{
+	time_t t;
+	struct tm *tmp;
+
+	t = time(NULL);
+	tmp = localtime(&t);
+
+	while (1) {
+		strftime(outstr, sizeof(outstr), argv[1], tmp
+		printf("Function called!\n");
+		sleep(); // Wait 5 seconds
+	}
+	return NULL;
+}
+
+void *handle_data(void *arg)
+{
+	char buffer[BUFFER_SIZE];
+	int fd = *(int *)arg;
+	int bytes_read;
+	char *found;
+
+	memset(buffer, 0, BUFFER_SIZE);
+	bytes_read = recv(fd, buffer, BUFFER_SIZE - 1, 0);
+
+	while (1) {
+		if (bytes_read <= 0) {
+			if (bytes_read == 0) {
+				syslog(LOG_DEBUG, "Client disconnected\n");
+			} else {
+				syslog(LOG_ERR, "Recv failed");
+			}
+			close(fd);
+			break;
+		} else {
+			found = strstr(buffer, "\n");
+			if (found) {
+				send(fd, filebuf, loc, 0);
+			} else {
+				// append to list
+			}
+		}
+	}
+
+	return NULL;
 }
 
 // Function to set a socket to non-blocking mode
-void set_nonblocking(int sockfd) {
-  int flags = fcntl(sockfd, F_GETFL, 0);
-  if (flags == -1) {
-    syslog(LOG_ERR, "fcntl F_GETFL");
-  }
-  if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
-    syslog(LOG_ERR, "fcntl F_SETFL");
-  }
+void set_nonblocking(int sockfd)
+{
+	int flags = fcntl(sockfd, F_GETFL, 0);
+	if (flags == -1) {
+		syslog(LOG_ERR, "fcntl F_GETFL");
+	}
+	if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+		syslog(LOG_ERR, "fcntl F_SETFL");
+	}
 }
 
-int main(int argc, char **argv) {
-  FILE *fptr;
-  int ret = 0;
-  int client_fd, max_fd;
-  struct sockaddr_in servaddr, client_addr;
-  int client_len = sizeof(client_addr);
-  struct hostent *hostp;
-  char *hostaddrp;
-  fd_set read_fds;
-  pid_t pid;
-  char buffer[BUFFER_SIZE];
+void start_timer()
+{
+	pthread_t thread_id;
 
-  openlog(NULL, LOG_NDELAY, LOG_USER | LOG_CONS);
+	// Create the thread
+	pthread_create(&thread_id, NULL, periodic_function, NULL);
 
-  signal(SIGINT, sig_handler);
-  signal(SIGTERM, sig_handler);
+	// Wait for the thread to finish (in this case, it runs indefinitely)
+	pthread_join(thread_id, NULL);
+}
 
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd == -1) {
-    syslog(LOG_ERR, "socket not created.");
-    ret = -1;
-    goto exit;
-  }
+int main(int argc, char **argv)
+{
+	int ret = 0;
+	int client_fd, max_fd;
+	struct sockaddr_in servaddr, client_addr;
+	int client_len = sizeof(client_addr);
+	struct hostent *hostp;
+	char *hostaddrp;
+	fd_set read_fds;
+	pid_t pid;
+	char buffer[BUFFER_SIZE];
 
-  set_nonblocking(server_fd);
+	openlog(NULL, LOG_NDELAY, LOG_USER | LOG_CONS);
 
-  bzero(&servaddr, sizeof(servaddr));
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
 
-  // assign IP, PORT
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(9000);
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+		syslog(LOG_ERR, "socket not created.");
+		ret = -1;
+		goto exit;
+	}
 
-  if ((bind(server_fd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
-    syslog(LOG_ERR, "Socket bind failed.");
-    ret = -1;
-    goto close_soc;
-  }
+	set_nonblocking(server_fd);
 
-  if (argc == 2 && argv[1][1] == 'd') {
-    pid_t pid = fork();
+	bzero(&servaddr, sizeof(servaddr));
 
-    if (pid < 0) {
-      syslog(LOG_ERR, "Fork failed exiting");
-      ret = -1;
-      goto close_soc;
-    } else if (pid > 0) {
-      // parent
-      goto close_soc;
-    }
-  }
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(9000);
 
-  fptr = fopen(filepath, "w");
+	if ((bind(server_fd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+		syslog(LOG_ERR, "Socket bind failed.");
+		ret = -1;
+		goto close_soc;
+	}
 
-  if (fptr == NULL) {
-    syslog(LOG_ERR, "file not opened");
-    ret = 1;
-    goto close_soc;
-  }
+	if (argc == 2 && argv[1][1] == 'd') {
+		pid_t pid = fork();
 
-  if (listen(server_fd, 5) < 0) {
-    syslog(LOG_ERR, "ERROR on listen");
-  }
+		if (pid < 0) {
+			syslog(LOG_ERR, "Fork failed exiting");
+			ret = -1;
+			goto close_soc;
+		} else if (pid > 0) {
+			// parent
+			goto close_soc;
+		}
+	}
 
-  // Initialize the fd sets
-  FD_ZERO(&active_fds);
-  FD_SET(server_fd, &active_fds);
-  max_fd = server_fd;
+	fptr = fopen(filepath, "w");
 
-  while (1) {
+	if (fptr == NULL) {
+		syslog(LOG_ERR, "file not opened");
+		ret = 1;
+		goto close_soc;
+	}
 
-    read_fds = active_fds;
+	if (listen(server_fd, 5) < 0) {
+		syslog(LOG_ERR, "ERROR on listen");
+	}
 
-    // Use select to wait for events
-    if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
-      syslog(LOG_ERR, "Select failed");
-      ret = 1;
-      goto close_file;
-    }
+	// Initialize the fd sets
+	FD_ZERO(&active_fds);
+	FD_SET(server_fd, &active_fds);
+	max_fd = server_fd;
 
-    for (int fd = 0; fd <= max_fd; fd++) {
-      if (FD_ISSET(fd, &read_fds)) {
-        if (fd == server_fd) {
-          // New connection
-          client_fd =
-              accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-          if (client_fd < 0) {
-            syslog(LOG_ERR, "Accept failed");
-            continue;
-          }
+	while (1) {
 
-          printf("New connection from %s:%d\n", inet_ntoa(client_addr.sin_addr),
-                 ntohs(client_addr.sin_port));
-          syslog(LOG_DEBUG, "Accepted connection from  %s:%d",
-                 inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-          // Set the new client socket to non-blocking mode
-          set_nonblocking(client_fd);
+		read_fds = active_fds;
 
-          // Add the new client socket to the active set
-          FD_SET(client_fd, &active_fds);
-          if (client_fd > max_fd) {
-            max_fd = client_fd;
-          }
-        } else {
-          // Handle data from a client
-          memset(buffer, 0, BUFFER_SIZE);
-          int bytes_read = recv(fd, buffer, BUFFER_SIZE - 1, 0);
-          if (bytes_read <= 0) {
-            if (bytes_read == 0) {
-              syslog(LOG_DEBUG, "Client disconnected\n");
-            } else {
-              syslog(LOG_ERR, "Recv failed");
-            }
-            close(fd);
-            FD_CLR(fd, &active_fds);
-          } else {
-            memcpy(filebuf + loc, buffer, bytes_read);
-            loc += bytes_read;
-            // printf("received data %s\n", buffer);
-            if (loc != 0 && filebuf[loc - 1] == '\n') {
-              send(fd, filebuf, loc, 0);
-            }
-            fprintf(fptr, "%s", buffer);
-            fflush(fptr);
-          }
-        }
-      }
-    }
-  }
+		// Use select to wait for events
+		if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
+			syslog(LOG_ERR, "Select failed");
+			ret = 1;
+			goto close_file;
+		}
+
+		for (int fd = 0; fd <= max_fd; fd++) {
+			if (FD_ISSET(fd, &read_fds)) {
+				if (fd == server_fd) {
+					// New connection
+					client_fd =
+						accept(server_fd, (struct sockaddr *)&client_addr,
+						       &client_len);
+					if (client_fd < 0) {
+						syslog(LOG_ERR, "Accept failed");
+						continue;
+					}
+
+					printf("New connection from %s:%d\n",
+					       inet_ntoa(client_addr.sin_addr),
+					       ntohs(client_addr.sin_port));
+					syslog(LOG_DEBUG, "Accepted connection from  %s:%d",
+					       inet_ntoa(client_addr.sin_addr),
+					       ntohs(client_addr.sin_port));
+					// Set the new client socket to non-blocking mode
+					set_nonblocking(client_fd);
+
+					// Add the new client socket to the active set
+					FD_SET(client_fd, &active_fds);
+					if (client_fd > max_fd) {
+						max_fd = client_fd;
+					}
+				} else {
+					// Handle data from a client
+					memset(buffer, 0, BUFFER_SIZE);
+					int bytes_read = recv(fd, buffer, BUFFER_SIZE - 1, 0);
+					if (bytes_read <= 0) {
+						if (bytes_read == 0) {
+							syslog(LOG_DEBUG, "Client disconnected\n");
+						} else {
+							syslog(LOG_ERR, "Recv failed");
+						}
+						close(fd);
+						FD_CLR(fd, &active_fds);
+					} else {
+						memcpy(filebuf + loc, buffer, bytes_read);
+						loc += bytes_read;
+						// printf("received data %s\n", buffer);
+						if (loc != 0 && filebuf[loc - 1] == '\n') {
+							send(fd, filebuf, loc, 0);
+						}
+						fprintf(fptr, "%s", buffer);
+						fflush(fptr);
+					}
+				}
+			}
+		}
+	}
 
 close_file:
-  fclose(fptr);
-  unlink(filepath);
+	fclose(fptr);
+	unlink(filepath);
 close_soc:
-  for (int fd = 0; fd < FD_SETSIZE; fd++) {
-    if (FD_ISSET(fd, &active_fds)) {
-      close(fd);
-    }
-  }
+	for (int fd = 0; fd < FD_SETSIZE; fd++) {
+		if (FD_ISSET(fd, &active_fds)) {
+			close(fd);
+		}
+	}
 
-  // Close the server socket
-  close(server_fd);
+	// Close the server socket
+	close(server_fd);
 
 exit:
-  closelog();
-  return ret;
+	closelog();
+	return ret;
 }
---
+++ server/main2.c
@@ -0,0 +1,306 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/syslog.h>
+#include <sys/types.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <sys/queue.h>
+
+#define SuccessOrExit(param)                                                                       \
+	do {                                                                                       \
+		if ((param) != 0) {                                                                \
+			goto exit;                                                                 \
+		}                                                                                  \
+	} while (0)
+
+#define VerifyOrExit(condition)                                                                    \
+	do {                                                                                       \
+		if (!(condition)) {                                                                \
+			goto exit;                                                                 \
+		}                                                                                  \
+	} while (0)
+
+void insert_file(char *buf, size_t size);
+
+struct file_emulator {
+	char *data;
+	LIST_ENTRY(file_emulator) entries;
+};
+
+LIST_HEAD(listhead, file_emulator);
+
+struct listhead file_head;
+
+int terminate = 0;
+
+pthread_mutex_t mutex;
+
+void sig_handler(int param)
+{
+	terminate = 1;
+}
+
+void config_signals()
+{
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+}
+
+int handle_options(int argc, char **argv)
+{
+	int opt;
+	pid_t pid;
+
+	while ((opt = getopt(argc, argv, "d")) != -1) {
+		switch (opt) {
+		case 'd':
+			pid = fork();
+
+			if (pid < 0) {
+				syslog(LOG_ERR, "Fork failed exiting");
+				return -1;
+			} else if (pid > 0) {
+				syslog(LOG_ERR, "Fork failed exiting");
+				return -2;
+			}
+			break;
+		default:
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+void *periodic_function(void *arg)
+{
+	time_t t;
+	struct tm *tmp;
+	char buf[64];
+
+	t = time(NULL);
+	tmp = localtime(&t);
+
+	while (!terminate) {
+		size_t size = strftime(buf, sizeof(buf), "timestamp:%Y:%m:%d:%H:%M:%S\n", tmp);
+		insert_file(buf, size);
+		printf("Function called! Time: %s\n", buf);
+		sleep(10);
+	}
+	return NULL;
+}
+
+void set_nonblocking(int sockfd)
+{
+	int flags = fcntl(sockfd, F_GETFL, 0);
+	if (flags == -1) {
+		syslog(LOG_ERR, "fcntl F_GETFL");
+	}
+	if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+		syslog(LOG_ERR, "fcntl F_SETFL");
+	}
+}
+
+int create_server_socket()
+{
+	int error = 0;
+	struct sockaddr_in servaddr;
+
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (server_fd == -1) {
+		syslog(LOG_ERR, "socket not created.");
+		printf("socket not created.");
+		error = -1;
+		goto exit;
+	}
+
+	set_nonblocking(server_fd);
+
+	bzero(&servaddr, sizeof(servaddr));
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(9000);
+
+	if ((bind(server_fd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+		syslog(LOG_ERR, "Socket bind failed.");
+		printf("Socket bind failed.");
+		error = -1;
+		goto close_soc;
+	}
+
+	if (listen(server_fd, 5) < 0) {
+		syslog(LOG_ERR, "ERROR on listen");
+		error = -1;
+		goto close_soc;
+	}
+
+	goto exit;
+
+close_soc:
+	close(server_fd);
+
+exit:
+	if (error != 0) {
+		return error;
+	} else {
+		return server_fd;
+	}
+}
+
+void insert_file(char *buf, size_t size)
+{
+	struct file_emulator *elem = malloc(sizeof(struct file_emulator));
+	elem->data = malloc(size + 1);
+
+	memcpy(elem->data, buf, size);
+	elem->data[size] = 0;
+
+	pthread_mutex_lock(&mutex);
+	if (LIST_EMPTY(&file_head)) {
+		LIST_INSERT_HEAD(&file_head, elem, entries);
+	} else {
+		struct file_emulator *last = LIST_FIRST(&file_head);
+		while (LIST_NEXT(last, entries) != NULL) {
+			last = LIST_NEXT(last, entries);
+		}
+		// Insert after the last element
+		LIST_INSERT_AFTER(last, elem, entries);
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+int send_all(int fd)
+{
+	struct file_emulator *iter;
+
+	pthread_mutex_lock(&mutex);
+	LIST_FOREACH(iter, &file_head, entries)
+	{
+		if (send(fd, iter->data, strlen(iter->data), 0) < 0) {
+			return -1;
+		}
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+void free_file()
+{
+	struct file_emulator *iter;
+	while (!LIST_EMPTY(&file_head)) {
+		iter = LIST_FIRST(&file_head);
+		free(iter->data);
+		LIST_REMOVE(iter, entries);
+		free(iter);
+	}
+}
+
+void *handle_client(void *args)
+{
+	int *fd = (int *)args;
+	char buf[3000000];
+	size_t pos = 0;
+
+	bzero(buf, sizeof(buf));
+
+	printf("Client FD: %d \n", *fd);
+
+	while (!terminate) {
+		int bytes_read = recv(*fd, buf, sizeof(buf) - pos, 0);
+		if (bytes_read > 0) {
+			printf("Received %s\n", buf);
+
+			pos += bytes_read;
+			if (buf[pos - 1] == '\n') {
+				insert_file(buf, pos);
+				send_all(*fd);
+				bzero(buf, sizeof(buf));
+				pos = 0;
+			}
+		} else {
+			printf("client disconnected\n");
+			break;
+		}
+	}
+
+exit:
+	close(*fd);
+	free(fd);
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	int error = 0;
+	int server_fd = -1;
+	int client_fd;
+	pthread_t thread_id[80];
+	int maxclient = 0;
+	int fds[20];
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+
+	openlog(NULL, LOG_NDELAY, LOG_USER | LOG_CONS);
+
+	config_signals();
+
+	pthread_mutex_init(&mutex, NULL);
+	VerifyOrExit(server_fd = create_server_socket());
+
+	SuccessOrExit(error = handle_options(argc, argv));
+
+	SuccessOrExit(error = pthread_create(thread_id, NULL, periodic_function, NULL));
+	maxclient++;
+
+	LIST_INIT(&file_head);
+
+	while (!terminate) {
+		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+		if (client_fd < 0) {
+			// syslog(LOG_ERR, "Accept failed");
+			// printf("Accept failed");
+			continue;
+		}
+		int *fd = malloc(sizeof(client_fd));
+		*fd = client_fd;
+		printf("New connection from %s:%d\n", inet_ntoa(client_addr.sin_addr),
+		       ntohs(client_addr.sin_port));
+
+		syslog(LOG_DEBUG, "Accepted connection from  %s:%d",
+		       inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+		printf("Spawning thread");
+		SuccessOrExit(error = pthread_create(&thread_id[maxclient], NULL, handle_client,
+						     (void *)fd));
+		maxclient++;
+	}
+
+	printf("Program terminated normally.\n");
+
+	for (size_t i = 0; i < maxclient; i++) {
+		SuccessOrExit(error = pthread_cancel(thread_id[i]));
+		SuccessOrExit(error = pthread_join(thread_id[i], NULL));
+	}
+	pthread_mutex_destroy(&mutex);
+exit:
+	if (server_fd >= 0) {
+		close(server_fd);
+	}
+	free_file();
+	if (error == -2) {
+		exit(0);
+	}
+	exit(error);
+}
